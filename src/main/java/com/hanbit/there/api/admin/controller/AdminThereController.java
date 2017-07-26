cec413bd47e66887c022782d5e7c7a7eaecc278b
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-26 12:27:32 +0900

    update listTheres api

--- src/main/java/com/hanbit/there/api/admin/controller/AdminThereController.java
+++ src/main/java/com/hanbit/there/api/admin/controller/AdminThereController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hanbit.there.api.admin.service.AdminThereService;
 import com.hanbit.there.api.admin.vo.AdminThereGroupVO;
+import com.hanbit.there.api.admin.vo.AdminThereVO;
 
 @RestController
 @RequestMapping("/api/admin/there")
@@ -23,27 +24,27 @@ public class AdminThereController {
 
 	@Autowired
 	private AdminThereService adminThereService;
-	
+
 	@RequestMapping("/groups")
 	public List<AdminThereGroupVO> listThereGroups() {
 		return adminThereService.listThereGroups();
 	}
-	
+
 	@RequestMapping("/group/order")
 	public List<AdminThereGroupVO> modifyThereGroupOrder(
 			@RequestParam("idUp") String idUp,
 			@RequestParam("idDown") String idDown) {
-		
+
 		adminThereService.modifyThereGroupOrder(idUp, idDown);
-		
+
 		return adminThereService.listThereGroups();
 	}
-	
+
 	@GetMapping("/group/{id}")
 	public AdminThereGroupVO getThereGroup(@PathVariable("id") String id) {
 		return adminThereService.getThereGroup(id);
 	}
-	
+
 	@PostMapping("/group/{id}")
 	public Map modifyThereGroup(@PathVariable("id") String id,
 			@RequestParam("name") String name) {
@@ -52,49 +53,61 @@ public class AdminThereController {
 		groupVO.setName(name);
 
 		adminThereService.modifyThereGroup(groupVO);
-		
+
 		Map result = new HashMap();
 		result.put("ok", true);
-		
+
 		return result;
 	}
-	
+
 	@DeleteMapping("/group/{id}")
 	public Map removeThereGroup(@PathVariable("id") String id) {
 		adminThereService.removeThereGroup(id);
 
 		Map result = new HashMap();
 		result.put("ok", true);
-		
+
 		return result;
 	}
-	
+
 	@PostMapping("/group/add")
 	public Map addThereGroup(@RequestParam("id") String id,
 			@RequestParam("name") String name) {
 		AdminThereGroupVO groupVO = new AdminThereGroupVO();
 		groupVO.setId(id);
 		groupVO.setName(name);
-		
+
 		adminThereService.addThereGroup(groupVO);
-		
+
 		Map result = new HashMap();
 		result.put("ok", true);
-		
+
 		return result;
 	}
-	
+
 	@RequestMapping(value="/group/{id}", method=RequestMethod.OPTIONS)
 	public Map hasThereGroupId(@PathVariable("id") String id) {
 		boolean exists = adminThereService.hasThereGroupId(id);
-		
+
 		Map result = new HashMap();
 		result.put("exists", exists);
-		
+
 		return result;
 	}
-	
+
+	@RequestMapping("/list")
+	public List<AdminThereVO> listTheres(
+			@RequestParam("groupId") String groupId) {
+
+		return adminThereService.listTheres(groupId);
+	}
+
 }
+
+
+
+
+
 
 
 
